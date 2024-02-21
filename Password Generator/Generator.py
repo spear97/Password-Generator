f9d86143f8316931706a90498664bd8bf0482d9c
spear97 <72108425+spear97@users.noreply.github.com>
2024-02-21 11:21:34 -0600

    Update Generator.py

--- Password Generator/Generator.py
+++ Password Generator/Generator.py
@@ -1,18 +1,30 @@
 import random
 
 class Generator(object):
-    
-    #Constructor
-    def __init__(self):
+    """
+    This class defines a Password Generator.
+    """
 
-        #Valid Characters that Password can be
+    def __init__(self):
+        """
+        Constructor for the Generator class.
+        Initializes the valid characters that can be used in the generated password.
+        """
+
+        # String containing all valid characters for the password
         self.string = 'abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?'
 
-    #Generate Random Password
     def GeneratePassword(self, passlen):
+        """
+        Generate a random password of specified length.
 
+        Args:
+            passlen (int): Length of the password to generate.
+
+        Returns:
+            str: A randomly generated password of the specified length.
+        """
+
+        # Use random.sample to create a password of length passlen
+        # The password will be a random selection of characters from self.string
         return "".join(random.sample(self.string, passlen))
-
-
-
-
